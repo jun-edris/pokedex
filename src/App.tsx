c26a7cc30ade8beee56ec2ba62326831eac853b1
Junaire Edris Buico <juned.buico@gmail.com>
2023-06-25 04:18:57 +0800

    change the layout of pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -110,7 +110,7 @@ const App = (): JSX.Element => {
               ))}
             {(!filteredLoading || !filteredTypeLoading) && (
               <>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-4 mt-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mt-4">
                   {searchPokemon
                     ? filteredData?.map((pokemon, index) => (
                         <div
